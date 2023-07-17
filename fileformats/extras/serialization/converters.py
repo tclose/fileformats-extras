cb1f9bd03260e43c26b966b034593fa68ecc1259
Tom Close <tom.g.close@gmail.com>
2023-07-17 17:34:00 +1000

    combined all std extras into single package

---
+++ fileformats/extras/serialization/converters.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import typing as ty
+import tempfile
+import pydra.mark
+import pydra.engine.specs
+from fileformats.core import mark
+from fileformats.serialization import DataSerialization, Json, Yaml
+
+
+@mark.converter(target_format=Json, output_format=Json)
+@mark.converter(target_format=Yaml, output_format=Yaml)
+@pydra.mark.task
+@pydra.mark.annotate({"return": {"out_file": DataSerialization}})
+def convert_data_serialization(
+    in_file: DataSerialization, output_format: ty.Type[DataSerialization], out_dir: ty.Optional[Path] = None
+):
+    dct = in_file.load()
+    if out_dir is None:
+        out_dir = Path(tempfile.mkdtemp())
+    output_path = out_dir / (in_file.fspath.stem + output_format.ext)
+    return output_format.save_new(output_path, dct)
